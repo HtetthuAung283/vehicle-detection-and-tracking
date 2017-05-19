1ed04152fde6ad56ec71a3a563e31b49f4c35d55
avoge <alexander.vogel@prozesskraft.de>
2017-05-19 18:24:54 +0200

    improve tracking

--- lib/detection.py
+++ lib/detection.py
@@ -37,37 +37,42 @@ class Detection():
         '''
             use the history of uinassigned positions to detect a new vehicle
         '''
-        if len(self.history_positions) < 2:
+        if len(self.history_positions) < 3:
             return
         
-        maybe_history_positions_minus_2 = []
+        maybe_history_positions_minus_3 = []
         
         # start with all non-assigned positions 2 frames ago
-        for position_old2 in self.history_positions[-2]:
-            maybeVehicle = Vehicle(position_old2)
+        for positions_minus_3 in self.history_positions[-3]:
+            maybeVehicle = Vehicle(positions_minus_3)
             
-            maybeVehicleConfirmed2 = False
+            maybeVehicleConfirmed_minus_3 = False
             
-            # try to confirm this vehicle in 1 frames ago
-            maybeVehicleConfirmed, maybe_history_positions_minus_1 = maybeVehicle.confirmVehicle(self.history_positions[-1])
+            # try to confirm this vehicle in 2 frames ago
+            maybeVehicleConfirmed_minus_2, maybe_history_positions_minus_2 = maybeVehicle.confirmVehicle(self.history_positions[-2])
             
             # if vehicle has been confirmed
-            if maybeVehicleConfirmed:
-                # try to confirm this vehicle in current frame
-                maybeVehicleConfirmed2, maybe_positions = maybeVehicle.confirmVehicle(self.positions)
+            if maybeVehicleConfirmed_minus_2:
+                # try to confirm this vehicle in 1 frame ago
+                maybeVehicleConfirmed_minus_1, maybe_history_positions_minus_1 = maybeVehicle.confirmVehicle(self.history_positions[-1])
         
                 # if vehicle has been confirmed again, then this vehicle will be added to the detected vehicle list
                 # and the old unassigned positions get updated
-                if maybeVehicleConfirmed2:
+                if maybeVehicleConfirmed_minus_1:
+                    # try to confirm this vehicle in current frame
+                    maybeVehicleConfirmed, maybe_positions = maybeVehicle.confirmVehicle(self.positions)
+
                     self.vehicles.append(maybeVehicle)
+                    self.history_positions[-2] = maybe_history_positions_minus_2
                     self.history_positions[-1] = maybe_history_positions_minus_1
                     self.positions = maybe_positions
+
             
             # if vehicle has not been confirmed through these 3 frames, then the old positions remain unchanged
-            if not maybeVehicleConfirmed2:
-                maybe_history_positions_minus_2.append(position_old2)
+            if not maybeVehicleConfirmed_minus_3:
+                maybe_history_positions_minus_3.append(positions_minus_3)
         
-        self.history_positions[-2] = maybe_history_positions_minus_2
+        self.history_positions[-3] = maybe_history_positions_minus_3
 
     def detect(self):
         self.confirmExistentVehicles()
